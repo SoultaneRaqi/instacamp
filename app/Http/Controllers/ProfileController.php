9f76cf447bf80c215a4fe5c3ab913c89b3a9328c
SoultaneRaqi <soultaneraqi1@gmail.com>
2025-11-29 16:49:26 +0100

    Implements the ProfileController update and edit  to manage user profile.

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+class ProfileController extends Controller
+{
+    public function index(User $user): View
+    {
+       return view(view:'profile.index', data: compact(var_name:'user')); 
+    }
+
+    public function edit(User $user): View
+    {
+        if (auth()->id() !== $user) {
+            abort(code:403 ,  message:'Unauthorized action.');
+        }
+
+        return view(view:'profiles.edit', data: compact(var_name:'user')); 
+    }
+
+    public function update(Request $request, User $user): RedirectResponse
+    {
+         if (auth()->id() !== $user) {
+            abort(code:403 ,  message:'Unauthorized action.');
+        }
+
+        $data = $request->validate(rules: [
+            'name' => 'required',
+            'username' => 'required',
+            'bio' => 'nullable',
+            'profile_image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        
+        if ($request->hasFile(key:'profile_image')) {
+            if ($user->profile_image) {
+                Storage::delete(path:$user->profile_image);
+            }
+            $imagePath = $request->file(key:'profile_image')->store(path:'uploads', options:'public');
+            $data['profile_image'] = basename(path:$imagePath);
+        }
+
+        $user->update(attributes: $data);
+        return redirect(to:'/profile/' . $user->id);
+    }
+}
+
+
