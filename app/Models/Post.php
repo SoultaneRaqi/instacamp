e08d56b133705a8102713172ab3b503694959e9a
SoultaneRaqi <soultaneraqi1@gmail.com>
2025-11-29 15:36:55 +0100

    Working on model Posts and seting relation belongs to User and has many likes and Comments

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $connection = 'mongodb';
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'caption',
+        'image_path',
+        'image_url',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(related: Comment::class);
+    }
+
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(related: Like::class);
+    }
+}
